b466bc9bb6136cca0a5842e02f25907385f925d0
Ahmedrali1 <ahmedrali97@gmail.com>
2024-02-01 16:51:36 +0300

    done

--- answer.js
+++ answer.js
@@ -1,18 +1,50 @@
 // create 5 burgers (at least 3 should be beef)
+db.burgers.insertMany([
+  {
+    protein: 'Beef',
+    cheese: true,
+    toppings: ['Olives', 'Mushrooms', 'Apple-slice']
+  },
+  {
+    protein: 'Beef',
+    cheese: true,
+    toppings: ['Lettuce', 'Mushrooms', 'Pineapple-slice']
+  },
+  {
+    protein: 'Beef',
+    cheese: true,
+    toppings: ['Lettuce', 'Mushrooms', 'Onion']
+  },
+  {
+    protein: 'Chicken',
+    cheese: true,
+    toppings: ['Tomato', 'Lettuce', 'Onion']
+  },
+  {
+    protein: 'Camel',
+    cheese: true,
+    toppings: ['Garlic', 'Red Chilli', 'Onion']
+  }
+])
 
 // find all the burgers
+db.burgers.find({})
 
 // show just the meat of each burger
+db.burgers.find({ protein: 'Beef' })
 
 // show just the toppings of each burger
+db.burgers.find({}, { _id: 0, toppings: 1 })
 
 // show everything but the cheese
 
 // find all the burgers with beef
+db.burgers.find({ protein: 'Beef' })
 
 // find all the burgers that are not beef
 
 // find the first burger with cheese
+db.burgers.find({ chease: 'Yes' })
 
 // find one and update the first burger with cheese to have a property of 'double cheese'
 
@@ -48,4 +80,4 @@
 // add a topping of 'eggs' to all the beef burgers
 //note since this db is 'reset' there should be no veggie burgers, all beef burgers should still be intact
 
-//Add a price to each burger, start with $5.00 for each burger 
+//Add a price to each burger, start with $5.00 for each burger
