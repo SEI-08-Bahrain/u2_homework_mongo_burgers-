e6e48eea45c459f42a79741030f6bda51365dded
Hamad-Alfandi <Hamad.alfandi7@gmail.com>
2024-02-04 09:00:15 +0300

    complete HW

--- answer.js
+++ answer.js
@@ -1,31 +1,55 @@
 // create 5 burgers (at least 3 should be beef)
 
-// find all the burgers
+db.burger.insertMany([
+  { protein: "beef", cheese: true, topping: "onions" },
+  { protein: "chicken", cheese: true, topping: "mustard" },
+  { protein: "turkey", cheese: false, topping: "french fries" },
+  { protein: "ostrich", cheese: false, topping: "ketchup" },
+  { protein: "tofu", cheese: true, topping: "olives" },
+  { protein: "buffalo", cheese: false, topping: "pickles" },
+])
 
-// show just the meat of each burger
+// find all the burgers
+db.burger.find({})
+
+// show just the protein of each burger
+db.burger.distinct('protein')
 
 // show just the toppings of each burger
+db.burger.distinct('toppings')
 
-// show everything but the cheese
+// show everything but not the cheese
+db.burger.find({} , {protein: 1, topping: 1})
 
 // find all the burgers with beef
+db.burger.find({protein: 'beef'})
 
 // find all the burgers that are not beef
+db.burger.find({protein: {$ne: 'beef'}})
 
 // find the first burger with cheese
+db.burger.findOne({protein: 'beef'})
 
 // find one and update the first burger with cheese to have a property of 'double cheese'
+db.burger.findOneAndUpdate({cheese: true}, {$set: {cheese: 'double cheese'}})
 
 // find the burger you updated to have double cheese
+db.burger.findOne({cheese: 'double cheese'})
 
 // find and update all the beef burgers to be 'veggie'
+db.burger.findOneAndUpdate({protein: 'beef'}, {$set: {protein: 'veggie'}})
+
 
 // delete one of your veggie burgers
 // WRONG - dELETES ALL : db.burger.remove({meat: 'veggie'})
+db.burger.deleteOne({protein: 'veggie'})
+
 
 // drop the collection
 //Expected Output
 //true
+db.burger.drop()
+
 
 // drop the database
 //Expected Output
@@ -33,6 +57,8 @@
 //   "dropped": "burgers",
 //   "ok": 1
 // }
+db.dropDatabase()
+
 
 //
 // Bonus
@@ -48,4 +74,4 @@
 // add a topping of 'eggs' to all the beef burgers
 //note since this db is 'reset' there should be no veggie burgers, all beef burgers should still be intact
 
-//Add a price to each burger, start with $5.00 for each burger 
+//Add a price to each burger, start with $5.00 for each burger
